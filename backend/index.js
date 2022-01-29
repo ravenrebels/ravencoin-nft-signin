7f50ae0d0b264790c5f3900f40a58f5e02f85cb4
Dick Henrik Larsson <dicklarsson@gmail.com>
2022-01-29 13:12:26 +0100

    Add healthcheck for backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
 const admin = require("firebase-admin");
 const config = require("./ravenConfig.json");
 const axios = require("axios");
+
+console.info("Welcome Ravencoin lover!");
+healthcheck();
 // Fetch the service account key JSON file contents
 var serviceAccount = require("./firebaseServiceAccount.json");
 var firebaseConfig = require("../client/firebaseConfig.json");
@@ -48,7 +52,6 @@ const listener = async function (snapshot) {
       //Fetch metadata about the asset, so we can get a nice IPFS image
       const metadata = await rpc(config, "getassetdata", [userData.name]);
 
-
       //Now create a "verification by user id" object with all necesarry info
       const toSave = {
         address: userData.address,
@@ -59,11 +62,12 @@ const listener = async function (snapshot) {
         signature: userData.signature,
       };
       db.ref("verificationsbyuserid/" + userKey).update(toSave);
-    } 
+    }
     //OK we do NOT have all info needed to verify identity
     else if (Object.keys(rvnData).length === 1) {
       //Reset verification by user id
       db.ref("verificationsbyuserid/" + userKey).update({});
+
       const address = Object.keys(rvnData)[0];
 
       if (address !== userData.address) {
@@ -108,4 +112,43 @@ function rpc(config, method, params) {
 
   const rpcResponse = axios.post(config.rpcURL, data, options);
   return rpcResponse;
+}
+
+function healthcheck() {
+  //HEALTHCHECK
+  function checkThatFileExists(file) {
+    try {
+      require(file);
+    } catch (_) {
+      console.info("HEALTHCHECK");
+      console.error(
+        "Error, oops You need to create the file",
+        file,
+        "please read readme.md for instructions 🤍"
+      );
+      process.exit(1);
+    }
+  }
+  checkThatFileExists("./ravenConfig.json");
+  checkThatFileExists("./firebaseServiceAccount.json");
+  checkThatFileExists("../client/firebaseConfig.json");
+
+  //Check that we can make a successful call to Ravencoin node
+
+  rpc(config, "listaddressesbyasset", ["DRUNK"])
+    .catch((e) => {
+      console.log(e.response);
+      throw Error("Could not connect to Ravencoin node");
+    })
+    .catch((event) => {
+      console.error(
+        "Oops, something seems wrong, cant connect to Ravencoin node"
+      );
+      console.info("Your node might not be up and running with RPC enabled");
+      console.log(
+        "Perhaps you did not provide the correct username/password/URL"
+      );
+      console.info("Please check the content of ./ravenConfig.json");
+      process.exit(1);
+    });
 }
